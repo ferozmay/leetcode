2d79eca23a08d1fe4b248e01989e46f1a54e4d8c
Feroz Hassan <75996466+ferozmay@users.noreply.github.com>
2025-10-27 00:58:54 +0000

    added solutions

--- src/medium/15.py
+++ src/medium/15.py
@@ -6,6 +6,8 @@ such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
 Notice that the solution set must not contain duplicate triplets.
 """
+from typing import List
+
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
